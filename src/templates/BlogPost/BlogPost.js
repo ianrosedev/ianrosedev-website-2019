9ff69d01173fd168a916ee01bab47bd51eb323a6
Ian Rose <ianrosedev@gmail.com>
2019-05-28 22:24:08 -0400

    [WIP] - Blog post page

--- src/templates/BlogPost/BlogPost.js
+++ src/templates/BlogPost/BlogPost.js
@@ -1,6 +1,10 @@
+// Need dangerouslySetInnerHTML for blog body
+/* eslint-disable react/no-danger */
 import React from 'react';
 import { graphql } from 'gatsby';
+import Img from 'gatsby-image';
 import LayoutMain from '../../layouts/LayoutMain/LayoutMain';
+import LayoutBody from '../../layouts/LayoutBody/LayoutBody';
 import SEO from '../../components/SEO/SEO';
 import styles from './BlogPost.module.scss';
 
@@ -9,6 +13,19 @@ export const query = graphql`
     contentfulBlogPost(slug: { eq: $slug }) {
       slug
       title
+      publishDate(formatString: "MMMM DD, YYYY")
+      heroImage {
+        fluid(maxWidth: 3000, quality: 100) {
+          ...GatsbyContentfulFluid_noBase64
+        }
+        description
+      }
+      body {
+        childMarkdownRemark {
+          html
+        }
+      }
+      tags
     }
   }
 `;
@@ -18,11 +35,34 @@ const BlogPost = ({ data }) => {
 
   return (
     <LayoutMain>
-      <SEO title={post.title} />
-      <div className={styles.foo}>
-        <h1>{post.title}</h1>
-        <h2>{post.slug}</h2>
-      </div>
+      <LayoutBody>
+        <SEO title={post.title} />
+        <div className={styles.post}>
+          <div className={styles.header}>
+            <Img
+              fluid={post.heroImage.fluid}
+              alt={post.heroImage.description || 'No description'}
+              className={styles.heroImage}
+            />
+            <h1>{post.title}</h1>
+            <p className={styles.publishDate}>{post.publishDate}</p>
+          </div>
+          <div className={styles.content}>
+            <div
+              dangerouslySetInnerHTML={{
+                __html: post.body.childMarkdownRemark.html,
+              }}
+            />
+          </div>
+          <div className={styles.tags}>
+            <h4>Tags</h4>
+            {post.tags.map(tag => (
+              <span key={tag}>{tag} </span>
+            ))}
+          </div>
+        </div>
+        <div className={styles.emptyRow} aria-hidden='true' />
+      </LayoutBody>
     </LayoutMain>
   );
 };
